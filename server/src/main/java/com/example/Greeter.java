998179fc1cebb12902be5b7dd1c909bb544d738c
repositorioCodePozosEquipoSF <juancarlostrejo79@gmail.com>
2017-10-26 12:59:22 -0500

    Fixing #3 of the checkstyle warnings.

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,7 +11,7 @@ public class Greeter {
   public Greeter() {
 
   }
- 
+
   /**
    * @param someone is the person who is going to be greeted
    * @return is the greeting made
--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -1,5 +1,5 @@
 /**
- * file for java doc
+ * file for java doc.
  */
 package com.example;
 
